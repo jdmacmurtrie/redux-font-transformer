2ba8dedcc35d20a88cac1144dc6e71cf61f4e497
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-12-01 08:45:09 -0500

    add more merge conflict

--- components/clickable.js
+++ components/clickable.js
@@ -6,11 +6,7 @@ const Clickable = props => {
   }
 
   return(
-<<<<<<< HEAD
-    <div className={props.className} onClick={props.handleSizeChange}>
-=======
     <div className={props.className} onClick={clickHandler}>
->>>>>>> 19b33b461e124bb3f13b7258d195b868cca755bd
       {props.text}
     </div>
   )
